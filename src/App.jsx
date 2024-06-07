69961849718753e30c896585f58d4f7bde9bde4b
RcnonS <ratchanon.somprasong@gmail.com>
2024-06-07 16:22:44 +0700

    React Data Fetching Find a Book Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [bookName, setBookName] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleSearch = (event) => {
+    setBookSearch(event.target.value);
+  };
+
+  const getBookName = async (bookSearch) => {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`
+    );
+    setBookName(result.data.items);
+  };
+
+  useEffect(() => {
+    getBookName(bookSearch);
+  }, [bookSearch]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" onChange={handleSearch} value={bookSearch} />
+      {bookName.map((item) => {
+        return (
+          <ul key={item.volumeInfo.id}>
+            <li>{item.volumeInfo.title}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
