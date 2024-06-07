0fc836e6068ca357c2f55e409f66df1c7b60dc28
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-06-07 15:37:15 +0700

    exercise-teal-golf-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchList, setSearchList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const getSearchList = async (seachText) => {
+    try {
+      const result = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${seachText}`
+      );
+      console.log(result);
+      setSearchList(result.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSearchList(searchText);
+  }, [searchText]);
+
+  const handleChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Find A Book</h1>
+      <label htmlFor="search-text"></label>
+      <input
+        id="search-text"
+        type="text"
+        value={searchText}
+        onChange={handleChange}
+      />
+      <div className="book-list">
+        <ul>
+          {searchList.map((item) => (
+            <li>{item.volumeInfo.title}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
