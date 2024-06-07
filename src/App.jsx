579ea15a5e9f698d123bc56cdb4238768da6be63
Apirak Fakin <apirakfakin@gmail.com>
2024-06-07 14:51:33 +0700

    feat: add suggusted books

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [bookName, setBookName] = useState("a");
+  const [suggestBooks, setSuggestBooks] = useState([]);
+
+  useEffect(() => {
+    GetBook();
+  }, [bookName]);
+
+  const GetBook = async () => {
+    try {
+      let response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${bookName}`
+      );
+      let books = await response.data.items;
+      setSuggestBooks(books);
+      console.log("result", books);
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+
+  return (
+    <div className="App">
+      {/* start coding here */}
+      <h1>Find a book</h1>
+      <input type="text" onChange={(e) => setBookName(e.target.value)} />
+      <div>
+        {suggestBooks.map((item) => {
+          return <li key={item.id}>{item.volumeInfo.title}</li>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
